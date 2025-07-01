90d35f98c606f15a0eca0150db24c96681d70530
meowmeowmac <saisann.lone0111@gmail.com>
2025-07-01 18:50:17 +0800

    Initial commit

--- script.js
+++ script.js
@@ -55,15 +55,15 @@
 */
 const messages = [
     "Are you sure?",
-    "Really sure??",
+    "Koko ngo mr nw",
     "Are you positive?",
     "Pookie please...",
     "Just think about it!",
-    "If you say no, I will be really sad...",
+    "If you say no, KoKo will be really sad...",
     "I will be very sad...",
     "I will be very very very sad...",
     "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Just kidding, Love youuuuu ❤️"
 ];
 
 let messageIndex = 0;
